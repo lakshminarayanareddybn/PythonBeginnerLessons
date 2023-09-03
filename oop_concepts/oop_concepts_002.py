0f29324cc7dfa156f46bc031d29b4b8d241d0df6
Lakshminarayana reddy <lakshminarayana84.reddy@gmail.com>
2023-09-03 04:59:42 +0000

    Class problems

---
+++ oop_concepts/oop_concepts_002.py
@@ -0,0 +1,15 @@
+
+## create a class called MyCalculator
+
+class MyCalculator:
+    ## Add your code here
+    ## def add(num1, num2)
+    ## like add other methods as well
+
+
+## test the class developed
+# my_calc = MyCalculator()
+# my_calc.add(4,5)
+# my_calc.div(4,5)
+# my_calc.mul(4,5)
+# my_calc.div(4,5)
